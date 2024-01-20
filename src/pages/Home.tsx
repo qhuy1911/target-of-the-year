f33f85d68f10204e7718f7e76f7b1b73f00017af
Huy Nguyen <huynguyen@Nguyens-MacBook-Pro.local>
2024-01-20 16:05:52 +0700

    Remove CD and update UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthProvider';
+import { UserModel } from '../models';
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  const authContext = useContext(AuthContext);
+  const [user, setUser] = useState<UserModel | null>(null);
+
+  useEffect(() => {
+    if (authContext?.user) {
+      setUser(authContext?.user);
+    }
+  }, [authContext?.user]);
+
+  return (
+    <>
+      <h1 className=' text-2xl text-center font-semibold'>Home</h1>
+      <p>Welcome {user?.displayName}</p>
+    </>
+  );
 };
 
 export default Home;
